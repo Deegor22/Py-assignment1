36c8af50908e0ae2ade90a09a345202a20318fc4
Deegor22 <dharmikmota76@gmail.com>
2025-07-11 18:00:15 +0530

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -2,6 +2,6 @@
 N1=input("Enter your first Name: ")
 N2=input("Enter your second Name: ")
 a=("!")
-Fname=N1+ N2+a
+Fname=N1+ " " +N2+a
 print()
 print("Hello,",Fname,"Welcome to the Python Program.")
